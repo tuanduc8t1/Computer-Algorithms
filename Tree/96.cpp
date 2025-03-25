ed76ba9f4f44b260f4766351f394c3beb53fa931
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2025-03-25 14:54:15 -0500

    Create 96.cpp

---
+++ Tree/96.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        int dp[100005];
+        memset(dp, 0, sizeof(dp));        
+        dp[0] = dp[1] = 1;
+ 
+        for(int i = 2;i <= n; i++){
+            for(int j = 0;j < i; j++){
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+};
