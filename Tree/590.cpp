e0babda5cd0f571e4fc44cfa15ecb7a2f275d3e1
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2025-03-25 14:58:59 -0500

    Create 590.cpp

---
+++ Tree/590.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> result;
+    void dfs(Node* root){
+        if (root == NULL) {
+            return;
+        }
+        for(Node* child: root -> children) {
+            dfs(child);
+        }
+        
+        result.push_back(root -> val);
+        
+    }
+    
+        vector<int> postorder(Node* root) {
+        dfs(root);
+        return result;
+    }
+};
