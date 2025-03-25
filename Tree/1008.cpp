ba237d66b56261c7284c89c6e36b53862314a89b
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2025-03-25 15:02:25 -0500

    Create 1008.cpp

---
+++ Tree/1008.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int i = 0;
+    TreeNode* bstFromPreorder(vector<int>& preorder, int value = 1e9) {
+        if (i == preorder.size() || preorder[i] > value) return NULL;
+        
+        TreeNode* root = new TreeNode(preorder[i++]);
+        
+        root->left = bstFromPreorder(preorder, root->val);
+        root->right = bstFromPreorder(preorder, value);
+        
+        return root;
+    }
+};
