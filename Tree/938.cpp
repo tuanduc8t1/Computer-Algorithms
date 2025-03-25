b3431621d4bff601f3e8886d9e44cd2b3a88d6ba
tuanduc8t1 <166401438+tuanduc8t1@users.noreply.github.com>
2025-03-25 15:01:53 -0500

    Create 938.cpp

---
+++ Tree/938.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        int sum=0;
+        int getsum(TreeNode* root,int L,int R){
+            if(root){
+                    getsum(root->left,L,R);
+            if(root->val>=L && root->val<=R) sum+=root->val;
+                    getsum(root->right,L,R);
+        }
+        return sum;
+    }
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(!root)return 0;
+        return getsum(root,L,R);
+    }
+};
